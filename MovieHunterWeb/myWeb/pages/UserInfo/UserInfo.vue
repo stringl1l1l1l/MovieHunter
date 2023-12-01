34e0dc7b670223b8c2e16e8586c298bbdd9178e5
fy-coder <2482312879@qq.com>
2023-12-01 15:36:56 +0800

    done

--- MovieHunterWeb/myWeb/pages/UserInfo/UserInfo.vue
+++ MovieHunterWeb/myWeb/pages/UserInfo/UserInfo.vue
@@ -1,33 +1,34 @@
 <template>
-	<view>
+	<view class="main">
 		<meta name="referrer" content="no-referrer" />
 		<div>
 		        <div class="header">
 		            <div class="box">
 		                <nav class="nav_link">
 		                    <img src="../../static/home.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../recommendations/recommendations">首页</router-link>
-		                </nav>
+		        						 <text class="link_btn"  @click="handleClick('recommendations/recommendations')">首页</text>
+		                    </nav>
 		            </div>
 		            <div class="box">
 		                <nav class="nav_link">
 		                    <img src="../../static/movieTag.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../favorite/favorite">收藏夹</router-link>
+		        						 <text class="link_btn"  @click="handleClick('favorite/favorite')">收藏夹</text>
 		                </nav>
-							 
+		        			 
 		                <nav class="nav_link">
 		                    <img src="../../static/user.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../UserInfo/UserInfo">个人主页</router-link>
+		                    <text class="link_btn" @click="handleClick('UserInfo/UserInfo')">个人主页</text>
 		                </nav>
-							 
-							 <nav class="nav_link">
-							     <img src="../../static/exit.png" class="img_btn">
-							     <router-link class="link_btn" active-class="active" to="../Login/LoginByEmail">退出</router-link>
-							 </nav>
+		        			 
+		        			 <nav class="nav_link">
+		        			     <img src="../../static/exit.png" class="img_btn">
+		        			     <text class="link_btn" @click="handleClick('Login/LoginByEmail')">退出</text>
+		        			 </nav>
 		            </div>
 		        </div>
 		    </div>
 		
+
 		<u-cell-group>
 			<u-cell title="个人头像" >
 				<template #right-icon>
@@ -39,10 +40,24 @@
 					></u-avatar>
 				</template>
 			</u-cell>
+			<u-cell title="用户名" :value="userinfo.username"></u-cell>
 			<u-cell title="邮箱地址" :value="userinfo.email"></u-cell>
 			<u-cell title="收藏夹" arrow-direction="left" @click="showFavorite"></u-cell>
+			<u-cell title="点赞记录" arrow-direction="left" @click="showLike"></u-cell>
+			<u-cell title="评论记录" arrow-direction="left" @click="showComment"></u-cell>
 		</u-cell-group>
+		<button class="logout-button" type="warning" @click="Popup">修改用户名</button>
+		<button class="logout-button" type="warning" @click="changePwd">修改密码</button>
+		<button class="logout-button" type="warning" @click="deleteUser">注销账号</button>
 		
+		<view v-if="showPopup" class="overlay">
+				<div class="sub-window">
+					<div @click="closePopup()" class="close-button">&times;</div>
+					  <input type="text" v-model="inputText" placeholder="请输入新名称" style="margin-top: 10pt;">
+					  <button class="submit-button" @click="submitText" style="margin-top: 10pt;">提交</button>
+					
+				</div>
+		</view>
 	</view>
 </template>
 
@@ -51,7 +66,42 @@
 		data() {
 			return {
 				token:'',
-				userinfo:{}
+				userinfo:{},
+				isCreateShow:false,
+				showPopup: false,
+				inputText: '',
+				form:{
+							  userInfo:{
+											oldPwd:'',
+							  			  email:'',
+							  			  password:''
+							  },
+							  confirmPassword: '', 
+							  code: ''
+				},
+						 
+				rules: {
+					oldPwd: [
+					  { required: true, message: '请输入密码', trigger: 'blur' }
+					],
+				  password: [
+				    { required: true, message: '请输入密码', trigger: 'blur' },
+				    { min: 6, max: 16, message: '密码长度为6到16个字符', trigger: 'blur' }
+				  ],
+				  confirmPassword: [
+				    { required: true, message: '请再次输入密码', trigger: 'blur' },
+				   {
+				   							trigger: 'blur',
+				   							validator: (rule, value, callback) => {
+				   								if (value !== this.form.userInfo.password) {
+				   									callback(new Error('两次输入密码不一致'))
+				   								} else {
+				   									callback()
+				   								}
+				   							}
+				   						}
+				  ]
+				}
 			};
 		},
 		onLoad(){
@@ -80,7 +130,7 @@
 					{
 						console.log(res)
 						that.userinfo = res.data.data
-						
+						console.log(that.userinfo)
 					}else{
 						console.log(res)
 					}
@@ -91,17 +141,158 @@
 					title:"网络出错了",
 					icon:'fail'
 				})
-			}
-			
-			
-			
-		})
+			}})
 	},
 	methods:
 	{
+		handleClick(e){
+			uni.redirectTo({
+				url:'/pages/'+e
+			})
+		},
+		submitText(){
+			// 在这里处理提交逻辑
+			var that = this
+			// console.log(this.selectFavoriteId)
+			// console.log('提交的文本:', this.inputText);
+			if (that.inputText=="") {
+				that.inputText = "默认"
+			}
+				uni.request({
+					url:this.$BASE_URL.BASE_URL + 'user/updateUserById',
+					method:'PUT',
+					header:{token:that.token},
+					data:{
+						userId: that.userinfo.userId,
+						email: that.userinfo.email,
+						username: that.inputText,
+						password: that.userinfo.password
+					},
+					success(res) {
+						if(res.data.code==200){
+							that.closePopup();
+							uni.showToast({
+								title:'修改成功'
+							})
+							uni.request({
+								url:that.$BASE_URL.BASE_URL+'user/parseToken/'+that.token,
+								method:'GET',
+								success(res) {
+									if(res.data.code==200)
+									{
+										console.log(res)
+										that.userinfo = res.data.data
+										console.log(that.userinfo)
+									}else{
+										uni.showToast({
+											title:res.data.message,
+											icon:'error'
+										})
+									}
+							},
+							fail(res) {
+								console.log(res)
+								uni.showToast({
+									title:"网络出错了",
+									icon:'fail'
+								})
+							}})
+						}else{
+							that.closePopup();
+							uni.showToast({
+								title:res.data.message,
+								icon:'error'
+							})
+						}
+					},
+					complete(res) {
+						console.log(res)
+					}
+				})
+			this.inputText = '';
+		},
+		closePopup(){
+			this.showPopup = false
+		},
+		Popup(){
+			this.showPopup=true
+		},
+		change(){
+			var that=this
+			if(that.form.userInfo.oldPwd != that.userinfo.password){
+				this.isCreateShow=false
+				uni.showToast({
+				  title: '密码错误',
+				  icon:"error"
+				});
+			}else{
+				
+			}
+		},
+		changePwd(){
+			// this.isCreateShow=true
+			uni.redirectTo({
+				url:'/pages/Login/findPassword'
+			})
+		},
+		closeSubWindow_create(){
+			this.isCreateShow=false
+		},
+		deleteUser()
+		{
+			
+			var that = this
+			console.log(that.userinfo)
+			uni.showModal({
+			  title: '确认注销用户？',
+			  content:'该操作不可撤销！',
+			  success(res) {
+			    if (res.confirm) {
+					
+			      uni.request({
+			        url: that.$BASE_URL.BASE_URL + 'user/deleteUserById/' + that.userinfo.userId,
+			        method: 'DELETE',
+			        header: { token: that.token ,
+					},
+			        success(res) {
+			          if (res.data.code == 200) {
+			            
+						uni.removeStorageSync('token');
+						uni.redirectTo({
+							url:'/pages/Login/LoginByEmail'
+						})
+						uni.showToast({
+						  title: '注销成功'
+						});
+			          }else{
+						  uni.showToast({
+						      title: res.data.message,
+						  	icon: 'error'
+						  });
+					  }
+			          
+			        },
+			        complete(res) {
+			          console.log(res);
+			        }
+			      });
+			    }
+			  }
+			});
+		},
 		showFavorite(){
 			uni.navigateTo({
 				url:'/pages/favorite/favorite'
+			})
+		},
+		showLike(){
+			uni.navigateTo({
+				url:'/pages/UserInfo/like'
+			})
+		},
+		showComment(){
+			uni.navigateTo({
+				url:'/pages/UserInfo/comment'
 			})
 		}
 	}
@@ -109,6 +300,60 @@
 </script>
 
 <style>
+.close-button {
+  position: absolute;
+  top:1px;
+  right: 8px;
+  font-size: 36px;
+  color: #999;
+  cursor: pointer;
+  /* background-color: #333; */
+  border-width: 0%;
+}
+.sub-window {
+	position: relative;
+	width: 300px;
+	 height: 100px;
+	 background-color: #fff;
+	 border: 1px solid #ccc;
+	 padding: 16px;
+}
+.close-button {
+  position: absolute;
+  top:1px;
+  right: 8px;
+  font-size: 36px;
+  color: #999;
+  cursor: pointer;
+  border-width: 0%;
+}
+.overlay {
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.sub-window-create {
+	position: relative;
+	width: 400px;
+	 height: 250px;
+	 background-color: #fff;
+	 border: 1px solid #ccc;
+	 padding: 16px;
+}
+.main {
+	background-color: #f5f5f5;
+}
+.button-container {
+  display: flex;
+  justify-content: space-between;
+}
 .header{
 	background-image: url("../../static/title.png");
 	background-repeat: no-repeat;
--- MovieHunterWeb/myWeb/pages/favorite/favorite.vue
+++ MovieHunterWeb/myWeb/pages/favorite/favorite.vue
@@ -6,58 +6,118 @@
 		            <div class="box">
 		                <nav class="nav_link">
 		                    <img src="../../static/home.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../recommendations/recommendations">首页</router-link>
-		                </nav>
+		        						 <text class="link_btn"  @click="handleClick('recommendations/recommendations')">首页</text>
+		                    </nav>
 		            </div>
 		            <div class="box">
 		                <nav class="nav_link">
 		                    <img src="../../static/movieTag.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../favorite/favorite">收藏夹</router-link>
+		        						 <text class="link_btn"  @click="handleClick('favorite/favorite')">收藏夹</text>
 		                </nav>
-							 
+		        			 
 		                <nav class="nav_link">
 		                    <img src="../../static/user.png" class="img_btn">
-		                    <router-link class="link_btn" active-class="active" to="../UserInfo/UserInfo">个人主页</router-link>
+		                    <text class="link_btn" @click="handleClick('UserInfo/UserInfo')">个人主页</text>
 		                </nav>
-							 
-							 <nav class="nav_link">
-							     <img src="../../static/exit.png" class="img_btn">
-							     <router-link class="link_btn" active-class="active" to="../Login/LoginByEmail">退出</router-link>
-							 </nav>
+		        			 
+		        			 <nav class="nav_link">
+		        			     <img src="../../static/exit.png" class="img_btn">
+		        			     <text class="link_btn" @click="handleClick('Login/LoginByEmail')">退出</text>
+		        			 </nav>
 		            </div>
 		        </div>
 		    </div>
 		
 		
-		<u-list>
-					 <u-list-item v-for="favorite in favorites" :key="favorite.favoriteId">
-						 <u-row justify="between">
-						 								 <u-col span="5" @click="showFavoriteMovies(favorite.favoriteId)">
-						 								 	{{favorite.name}}
-						 								 </u-col>
-						 								 <u-col span="1">
-															 <u-button text="移除" type="warning" class="button" @click="deleteFavorite(favorite.favoriteId)"></u-button>
-														 </u-col>
-						 								 
-						 </u-row>
-						
-					 </u-list-item>
-		</u-list>.
+		<view class="main">
+		<view class="favorites-list">
+			<button @click="createBtn()" style="background-color: aliceblue; margin: 10pt;">新建收藏夹</button>
+		    <view v-for="favorite in favorites" :key="favorite.favoriteId" class="favorite-item">
+		      <view class="container" >
+		        <view class="left-section">
+					<text class="text" >{{ favorite.name }}</text></view>
+				  <view class="right-section">
+					  <u-button class="edit-button" @click="showFavoriteMovies(favorite.favoriteId)" text="查看"></u-button>
+						<u-button class="edit-button" @click="editItem(favorite.favoriteId)" text="修改"></u-button>
+						<u-button class="remove-button" text="移除" type="warning"  @click="deleteFavorite(favorite.favoriteId)"></u-button> 
+				  </view>
+		        
+		      </view>
+		    </view>
+		  </view>
+		</view> 
 		
-		<u-popup :show="isMoviesShow" :closeable="true" @close="close" closeIconPos="top-left" :safeAreaInsetTop="true">
-			<u-list class="movie-list">
-						 <u-list-item v-for="movie in movies" :key="movie.movieId">
-							 <u-row>
-								 <u-col span="5">
-								 	{{movie.name}}
-								 </u-col>
-								 <u-col span="1"></u-col>
-								 <u-button text="移除" type="warning" class="button" @click="deleteMovie(movie.movieId)"></u-button>
-							 </u-row>
-							
-						 </u-list-item>
-			</u-list>.
-		</u-popup>
+		<view v-if="isCreateShow" class="overlay">
+				<div class="sub-window-create">
+					<div @click="closeSubWindow_create()" class="close-button">&times;</div>
+					<u-input  v-model="favoriteName" placeholder="请输入收藏夹名称"></u-input>
+					<u-button @click="createFavorite()" style="margin-top: 10pt;">点击创建</u-button>
+					<view v-if="isErrorShow" style="color: red;">{{errorMessage}}</view>
+				</div>
+		</view>
+		
+		<view v-if="showPopup" class="overlay">
+				<div class="sub-window-create">
+					<div @click="closePopup()" class="close-button">&times;</div>
+						
+					  <input type="text" v-model="inputText" placeholder="请输入新名称" style="margin-top: 10pt;">
+					  <button class="submit-button" @click="submitText" style="margin-top: 10pt;">提交</button>
+					
+				</div>
+		</view>
+			
+		<view v-if="isMoviesShow" class="overlay">
+				<div class="sub-window-big">
+					<div @click="closeMoviesShow()" class="close-button">&times;</div>
+					<div class="scrollable-list" >
+					<view class="movie-list" style="margin-top: 10pt;">
+					  <view v-for="movie in movies" :key="movie.movieId" class="movie-item-small">
+						  <u-row>
+							  <u-col span="6">
+								  <text class="movie-name" >{{ movie.name }}</text>
+							  </u-col>
+							  <u-col span="3">
+								  <u-button text="移除" type="warning" class="button" @click="deleteMovie(movie.movieId)"></u-button>
+							  </u-col>
+							  <u-col span="3">
+									<u-button text="移动" type="primary" class="button" @click="changeMovie(movie.movieId)"></u-button>
+							  </u-col>
+						  </u-row>
+						
+					  </view>
+					</view>
+					</div>
+				</div>
+		</view>
+		
+		
+		  
+		<view v-if="showChange" class="overlay">
+				<div class="sub-window-big">
+					<div @click="closeShowChange()" class="close-button">&times;</div>
+					<div class="scrollable-list" >
+					<view class="movie-list" style="margin-top: 10pt;">
+						
+					  <view v-for="favorite in favorites" :key="favorite.favoriteId" class="movie-item-small">
+						  <u-row>
+							  <u-col span="9">
+								  <text class="movie-name" >{{ favorite.name }}</text>
+							  </u-col>
+							  <u-col span="3">
+									<u-button text="移动" type="primary" class="button" @click="confirmChange(favorite.favoriteId)"></u-button>
+							  </u-col>
+						  </u-row>
+					  </view>
+					  
+					</view>
+					</div>
+				</div>
+		</view>
+			
+	
+		
+		</view>
+		
 	</view>
 </template>
 
@@ -71,7 +131,13 @@
 				isMoviesShow:false,
 				token:'',
 				selectFavoriteId:0,
-				userinfo:{}
+				userinfo:{},
+				showChange:false,
+				selectMovieId:0,
+				showPopup: false,
+				inputText: '',
+				isCreateShow: false,
+				isErrorShow: false,
 			}
 		},
 		onLoad(){
@@ -112,155 +178,482 @@
 					title:"网络出错了",
 					icon:'fail'
 				})
-			}
-			
-			
-			
-		})},
+			}})
+			that.updateFavorite()
+		},   
 		methods: {
-			deleteFavorite(e)
-			{
-				console.log(e)
-				var that=this
-				uni.showModal({
-					title:"确认删除？",
-					success(res) {
-						if(res.confirm)
-						{
-							uni.request({
-								url:that.$BASE_URL.BASE_URL+'favorite/deleteFavoriteById/'+e,
-								method:'DELETE',
-								header:{token:that.token},
-								success(res) {
-									if(res.data.code==200)
-									{
-										uni.showToast({
-											title:"删除成功"
-										})
-									}
-									that.showFavorite()
-								},
-								complete(res) {
-										console.log(res)
-								}
-							})
-						}
-					}
+			handleClick(e){
+				uni.redirectTo({
+					url:'/pages/'+e
 				})
 			},
-			showFavorite()
-			{
-				var that = this
-				uni.request({
-								url:this.$BASE_URL.BASE_URL + 'favorite/insertFavorite/'+that.userId,
-								header:{token:that.token},
-								method:'GET',
+			updateFavorite() {
+					  var that = this
+					  uni.request({
+						url:this.$BASE_URL.BASE_URL+'favorite/insertFavorite/'+that.userId,
+							method:'GET',
+							header:{
+								token:that.token
+								},
 								success(res) {
-									if(res.data.code==200){
-										that.favorites = res.data.data
-									}else{
-										console.log(res)
-									}
+									console.log(res)
+									that.favorites = res.data.data
 								},
 								fail(res) {
-												console.log(res)
-												uni.showToast({
-													title:"网络出错了",
-													icon:'error'
-												})
-											},
-											complete(res) {
-												console.log(res)
-											}
-							})
+									console.log(res)
+									uni.showToast({
+										title:"网络出错了",
+										icon:'fail'
+									})
+								}
+					  })
+					// this.showSubWindow()
 			},
-			showFavoriteMovies(e)
+			showError(e) {
+					  this.isErrorShow = true
+					  this.errorMessage = e
+			},
+			createFavorite()
 			{
-				console.log(e)
-				var that=this
-				this.selectFavoriteId = e
+				var that = this
+				if (that.favoriteName=="") {
+					that.favoriteName = "默认"
+				}
 				uni.request({
-					url:this.$BASE_URL.BASE_URL + 'favorite/findAllMoviesInFavorite/'+e,
-					method:'GET',
+					url:this.$BASE_URL.BASE_URL + 'favorite/insertFavorite',
+					method:'PUT',
 					header:{token:that.token},
+					data:{
+						userId:that.userId,
+						name:that.favoriteName
+					},
 					success(res) {
-						if(res.data.code==200)
-						{
-							that.movies = res.data.data
-							if(res.data.data.length==0)
-							{
+							if (res.data.code != 200) {
+								that.showError(res.data.message)
+							}else{
+								that.favoriteName = ''
+								that.isCreateShow=false
+								that.updateFavorite()
 								uni.showToast({
-									title:"暂无电影",
-									icon:"error"
+									title:'创建成功'
+								})
+							}
+					},
+					fail(res) {
+							that.showError("网络出错了")
+					},
+				})
+			},
+			closeSubWindow_create(){
+					  this.isCreateShow = false
+					  this.isErrorShow=false
+			},
+			createBtn() {
+				var that = this
+				that.isCreateShow=true
+			},
+			confirmChange(e){
+					  console.log(e)
+					  var that=this
+					  uni.request({
+					  	url:this.$BASE_URL.BASE_URL + 'favorite/moveMovieToFavorite/' + e,
+						method:'POST',
+						header:{token:that.token},
+						data:{
+							favoriteId:that.selectFavoriteId,
+							movieId:that.selectMovieId
+						},
+						success(res) {
+							if(res.data.code==200){
+								that.closeShowChange()
+								that.closeMoviesShow()
+								uni.showToast({
+									title:'移动成功'
 								})
 							}
 							else{
-								that.isMoviesShow=true
+								that.closeShowChange()
+								that.closeMoviesShow()
+								uni.showToast({
+								  title: res.data.message,
+								  icon: 'error'
+								});
 							}
 							
-						}else{
-							console.log(res)
-						}
-					},
-					fail(res) {
-							console.log(res)
-							uni.showToast({
-								title:"网络出错了",
-									icon:'error'
-							})
-							},
-							complete(res) {
-								console.log(res)
-							}
-				})
+						},
+						fail(res) {
+							that.closeShowChange()
+						  that.closeMoviesShow()
+						  uni.showToast({
+						    title: '网络出错了',
+						    icon: 'error'
+						  });
+						},
+					  })
 			},
-			close() {
-				this.isMoviesShow = false
-			},
-			deleteMovie(e)
-			{
-				console.log(e)
-				var that=this
+			 
+			  changeMovie(e)
+			  {
+				  this.isMoviesShow = false
+			      this.showChange = true
+				  this.selectMovieId = e
+			  },
+			  editItem(e)
+			  {
+				  this.showPopup = true;
+				  this.selectFavoriteId = e
+				  console.log(this.userId)
+			  },
+			  submitText() {
+			  // 在这里处理提交逻辑
+			  var that = this
+			  console.log(this.selectFavoriteId)
+			  console.log('提交的文本:', this.inputText);
+			  if (that.inputText=="") {
+				  that.inputText = "默认"
+			  }
 				uni.request({
-					url:this.$BASE_URL.BASE_URL+'favorite/deleteMovieInFavorite',
-					method:'DELETE',
+					url:this.$BASE_URL.BASE_URL + 'favorite/updateFavouriteById',
+					method:'POST',
 					header:{token:that.token},
 					data:{
 						favoriteId:that.selectFavoriteId,
-						movieId:e
-						
+						userId:that.userId,
+						name:that.inputText
 					},
-					success(res){
-						if(res.data.code==200)
-						{
-							uni.showToast({
-								title:"移除成功"
-							})
-						}else{
-							console.log(res)
-						}
-						
-					},
-					fail(res) {
-							console.log(res)
-							uni.showToast({
-								title:"网络出错了",
-									icon:'error'
-							})
-							},
-							complete(res) {
-								console.log(res)
-							}
-					
-				}),
-				this.showFavoriteMovies(this.selectFavoriteId)
-			},
+					success(res) {
+						if(res.data.code==200){
+							that.showFavorite();
+							
 			
+							uni.showToast({
+								title:'修改成功'
+							})
+							
+						}else{
+							uni.showToast({
+								title:res.data.message,
+								icon:'error'
+							})
+						}
+					},
+					complete(res) {
+						console.log(res)
+					}
+				})
+			  // 提交完成后关闭弹出框
+			  this.showPopup = false;
+			  this.inputText = '';
+			},
+			deleteFavorite(e) {
+			  console.log(e);
+			  var that = this;
+			  uni.showModal({
+			    title: '确认删除？',
+			    success(res) {
+			      if (res.confirm) {
+			        uni.request({
+			          url: that.$BASE_URL.BASE_URL + 'favorite/deleteFavoriteById/' + e,
+			          method: 'DELETE',
+			          header: { token: that.token },
+			          success(res) {
+			            if (res.data.code == 200) {
+							that.showFavorite();
+			              uni.showToast({
+			                title: '删除成功'
+			              });
+			            }
+			          },
+			          complete(res) {
+			            console.log(res);
+			          }
+			        });
+			      }
+			    }
+			  });
+			},
+			showFavorite() {
+				
+			  var that = this;
+			  uni.request({
+			    url: this.$BASE_URL.BASE_URL + 'favorite/findFavoritesByCurUser/',
+			    header: { token: that.token },
+			    method: 'GET',
+			    success(res) {
+			      if (res.data.code == 200) {
+			        that.favorites = res.data.data;
+			      } else {
+			        console.log(res);
+			      }
+			    },
+			    fail(res) {
+			      console.log(res);
+			      uni.showToast({
+			        title: '网络出错了',
+			        icon: 'error'
+			      });
+			    },
+			    complete(res) {
+			      console.log(res);
+			    }
+			  });
+			},
+			showFavoriteMovies(e) {
+				
+			  console.log(e);
+			  var that = this;
+			  this.selectFavoriteId = e;
+			  uni.request({
+			    url: this.$BASE_URL.BASE_URL + 'favorite/findAllMoviesInFavorite/' + e,
+			    method: 'GET',
+			    header: { token: that.token },
+			    success(res) {
+			      if (res.data.code == 200) {
+			        that.movies = res.data.data;
+			        if (res.data.data.length == 0) {
+			          uni.showToast({
+			            title: '暂无电影',
+			            icon: 'error'
+			          });
+			        } else {
+			          that.isMoviesShow = true;
+			        }
+			      } else {
+			        console.log(res);
+			      }
+			    },
+			    fail(res) {
+			      console.log(res);
+			      uni.showToast({
+			        title: '网络出错了',
+			        icon: 'error'
+			      });
+			    },
+			    complete(res) {
+			      console.log(res);
+			    }
+			  });
+			},
+			closeMoviesShow() {
+			  this.isMoviesShow = false;
+			},
+			closeShowChange(){
+				console.log('关闭')
+				this.showChange = false
+			},
+			closePopup(){
+				this.showPopup = false
+			},
+			deleteMovie(e) {
+			  console.log(e);
+			  var that = this;
+			  uni.request({
+			    url: this.$BASE_URL.BASE_URL + 'favorite/deleteMovieInFavorite',
+			    method: 'DELETE',
+			    header: { token: that.token },
+			    data: {
+			      favoriteId: that.selectFavoriteId,
+			      movieId: e
+			    },
+			    success(res) {
+			      if (res.data.code == 200) {
+					that.closeMoviesShow()
+			        uni.showToast({
+			          title: '移除成功'
+			        });
+			      } else {
+			        that.closeMoviesShow()
+					uni.showToast({
+					  title: res.data.message,
+					  icon: 'error'
+					});
+			      }
+			    },
+			    fail(res) {
+			      that.closeMoviesShow()
+			      uni.showToast({
+			        title: '网络出错了',
+			        icon: 'error'
+			      });
+			    },
+			    complete(res) {
+					that.closeMoviesShow()
+			      console.log(res);
+			    }
+			  });
+			  this.showFavoriteMovies(this.selectFavoriteId);
+			}
 		}
+		
 	}
 </script>
 
 <style>
+.scrollable-list {
+  overflow-y: auto;
+  height: 100%;
+}
+.popup-content {
+	margin-top: 20pt;
+}
+.main {
+	display: flex;
+	justify-content: center;
+	background-color: #f5f5f5;
+}
+.close-button {
+  position: absolute;
+  top:1px;
+  right: 8px;
+  font-size: 36px;
+  color: #999;
+  cursor: pointer;
+  /* background-color: #333; */
+  border-width: 0%;
+}
+.close-button:hover {
+  color: #333;
+}
+.overlay {
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.sub-window {
+  position: relative;
+ width: 300px;
+  height: 315px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 16px;
+}
+.sub-window-create {
+	position: relative;
+	width: 300px;
+	 height: 100px;
+	 background-color: #fff;
+	 border: 1px solid #ccc;
+	 padding: 16px;
+}
+.sub-window-big {
+	position: relative;
+	width: 600px;
+	 height: 700px;
+	 background-color: #fff;
+	 border: 1px solid #ccc;
+	 padding: 16px;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+ 
+  padding: 10px;
+ 
+}
+.left-section {
+  flex: 1;
+}
+
+.favorites-list {
+	background-color: #f5f5f5;
+	width:50%
+}
+.remove-button {
+  color: #fff;
+  border: none;
+  
+  border-radius: 4px;
+  cursor: pointer;
+  min-width: 80px; /* 调整按钮的最小宽度 */
+   margin-left: 10px; /* 将按钮推至右侧 */
+  
+}
+.edit-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  
+  border-radius: 4px;
+  cursor: pointer;
+  min-width: 80px; /* 调整按钮的最小宽度 */
+   margin-left: 10px; /* 将按钮推至右侧 */
+}
+.favorite-item {
+  /* display: flex;
+   flex: 1 1 200px;
+  align-items: center; */
+  justify-content: space-between;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  padding: 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+
+.favorite-info {
+  display: flex;
+  align-items: center;
+}
+
+.favorite-name {
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+.button {
+  margin-left: 10px;
+}
+
+.movie-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  z-index: 0;
+}
+
+.movie-item {
+  width: 100%;
+  max-width: 600px;
+  border: 1px solid #e0e0e0;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  padding: 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+.movie-item-small {
+	width: 100%;
+	border: 1px solid #e0e0e0;
+	border-radius: 5px;
+	margin-bottom: 20px;
+	padding: 15px;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	background-color: #fff;
+}
+.text {
+  font-size: 16px;
+  white-space: nowrap; /* 防止文字换行 */
+  overflow: hidden; /* 超出部分隐藏 */
+  text-overflow: ellipsis; /* 超出部分显示省略号 */
+}
+.movie-name {
+  margin: 20px;
+}
+.right-section {
+  display: flex;
+  margin-left: 80px; /* 将右侧按钮推至右边缘 */
+}
 .header{
 	background-image: url("../../static/title.png");
 	background-repeat: no-repeat;
